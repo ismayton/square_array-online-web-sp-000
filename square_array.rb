976e51ec3dda1c184bc4b23710a7746de2382c2c
Ian Mayton <ian.mayton@gmail.com>
2020-07-16 16:22:30 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,11 @@
 def square_array(array)
   newArray = [];
-  array.each do |num|
+  array.collect do {|num|
     square = num*num
     newArray.push(square)
   end
   newArray
 end
 
-def square_array_collect(array)
+
   
